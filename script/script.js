9e9e6f58776dcf454baf3db6b5de4a93bf506955
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2018-03-28 16:15:10 -1000

    Updated fonts with Google Font APIs. Website is slightly more responsive

--- script/script.js
+++ script/script.js
@@ -1,24 +1,33 @@
 window.onscroll = function() {scrollFunction()};
 // window.addEventListener("click", changeTextColor());
 
+var size = {
+  width: window.innerWidth || document.body.clientWidth,
+ height: window.innerHeight || document.body.clientHeight
+}
+console.log(size);
+
 function scrollFunction() {
+if(size.width>=601){
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
   document.getElementById("navbar").style.top = "0px";
   } else {
     document.getElementById("navbar").style.top = "-100px";
   }
+  }
+}
+if(size.width<=600){
+  console.log(size.width);
+  myFunction();
 }
 
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
 
-// function changeTextColor(){
-//   var check  = document.getElementById("page1").value;
-//   alert(check);
-// }
+  alert("Page has changed..");
+    // var x = document.getElementById("navbar");
+    // if (x.className === "topnav") {
+    //     x.className += "responsive";
+    // } else {
+    //     x.className = "topnav";
+    // }
+}
