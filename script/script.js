f3111ca2012563d3a6a2f692028374c493022971
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2018-03-31 08:15:21 -1000

    Modified some styles and merged scripts into one file

--- script/script.js
+++ script/script.js
@@ -1,33 +1,44 @@
-window.onscroll = function() {scrollFunction()};
-// window.addEventListener("click", changeTextColor());
-
 var size = {
   width: window.innerWidth || document.body.clientWidth,
- height: window.innerHeight || document.body.clientHeight
+  height: window.innerHeight || document.body.clientHeight
 }
 console.log(size);
 
-function scrollFunction() {
-if(size.width>=601){
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  document.getElementById("navbar").style.top = "0px";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
+function checkDimension(){
+  console.log("Entering function..");
+  if(size.width<=600){
+    document.getElementById("navbar").style.top="0px";
+    alert(size.width);
+  }else{
+    scrollFunction();
   }
 }
-if(size.width<=600){
+
+window.onscroll = function() {
+  scrollFunction()
+};
+window.onload = checkDimension();
+
+function scrollFunction() {
+  if (size.width >= 601) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0px";
+    } else {
+      document.getElementById("navbar").style.top = "-100px";
+    }
+  }
+}
+if (size.width <= 600) {
   console.log(size.width);
-  myFunction();
 }
 
 function myFunction() {
 
   alert("Page has changed..");
-    // var x = document.getElementById("navbar");
-    // if (x.className === "topnav") {
-    //     x.className += "responsive";
-    // } else {
-    //     x.className = "topnav";
-    // }
+  // var x = document.getElementById("navbar");
+  // if (x.className === "topnav") {
+  //     x.className += "responsive";
+  // } else {
+  //     x.className = "topnav";
+  // }
 }
