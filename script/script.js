d7dc81335e73518d8fb222e8329980ad70424cbd
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2018-04-05 16:23:50 -1000

    Added images and made navbar slightly more mobile-friendly

--- script/script.js
+++ script/script.js
@@ -2,43 +2,21 @@ var size = {
   width: window.innerWidth || document.body.clientWidth,
   height: window.innerHeight || document.body.clientHeight
 }
+var modal = document.getElementById('modal-id');
+
+console.log(modal);
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 console.log(size);
 
-function checkDimension(){
-  console.log("Entering function..");
-  if(size.width<=600){
-    document.getElementById("navbar").style.top="0px";
-    alert(size.width);
-  }else{
-    scrollFunction();
+function phoneChange() {
+  var x = document.getElementById("phone-nav");
+  if (x.className === "navbar") {
+    x.className += "responsive";
+  } else {
+    x.className = "navbar";
   }
-}
-
-window.onscroll = function() {
-  scrollFunction()
-};
-window.onload = checkDimension();
-
-function scrollFunction() {
-  if (size.width >= 601) {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("navbar").style.top = "0px";
-    } else {
-      document.getElementById("navbar").style.top = "-100px";
-    }
-  }
-}
-if (size.width <= 600) {
-  console.log(size.width);
-}
-
-function myFunction() {
-
-  alert("Page has changed..");
-  // var x = document.getElementById("navbar");
-  // if (x.className === "topnav") {
-  //     x.className += "responsive";
-  // } else {
-  //     x.className = "topnav";
-  // }
 }
